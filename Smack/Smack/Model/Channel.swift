678105b94a988e7ede9ec7d136d9bd5d11329456
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-03-06 22:35:00 +0000

    Message decoding with SwiftyJSON

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Odhrán Daly on 06/03/2019.
+//  Copyright © 2019 Odhrán Daly. All rights reserved.
+//
+
+import Foundation
+
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
